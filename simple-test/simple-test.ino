094dbb115f8e5c7c1a36a9e7cc25a3e6f6f23e0f
drtorq <doc@drtorq.com>
2021-11-10 16:01:48 -0500

    2nd commit

--- simple-test/simple-test.ino
+++ simple-test/simple-test.ino
@@ -1,4 +1,21 @@
 
+/***************************************************
+  This is our GFX example for the Adafruit ILI9341 Breakout and Shield
+  ----> http://www.adafruit.com/products/1651
+
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+//Dr Torq - simple test file
+ 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
