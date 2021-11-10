571b2f2ce5eec905a731f9b61fbc8ac6bd8fd452
drtorq <doc@drtorq.com>
2021-11-10 15:35:37 -0500

    Initial commit of the esp8266-tft repo

---
+++ simple-test/simple-test.ino
@@ -0,0 +1,48 @@
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+// defined(ESP8266)
+  #define TFT_CS         4
+  #define TFT_RST        16                                            
+  #define TFT_DC         5
+
+// For 1.14", 1.3", 1.54", 1.69", and 2.0" TFT with ST7789:
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("Hello! ST77xx TFT Test");
+
+tft.init(240, 320);           // Init ST7789 320x240    
+Serial.println("Initialized");
+
+// tft print function!
+  tftPrintTest();
+  Serial.println("Process Completed");
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Serial.println("Process Completed");
+
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 200);
+  //tft.setTextColor(ST77XX_RED);
+  //tft.setTextSize(1);
+  //tft.println("Hello World!");
+  //tft.setTextColor(ST77XX_YELLOW);
+  //tft.setTextSize(2);
+  //tft.println("Hello World!");
+  
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  }
